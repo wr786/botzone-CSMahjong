a6c227915df7bf2635baea068195057946d8e3fa
wr786 <wr786@pku.edu.cn>
2020-04-20 14:02:20 +0800

    新增了用于便捷调参的小工具quickTuning

---
+++ project/src/quickTuning.py
@@ -0,0 +1,24 @@
+
+file = open('ScoreCalculator.cpp', 'r', encoding="utf-8")
+lines = file.read().splitlines()
+file.close()
+
+file = open('ScoreCalculator.cpp', 'w', encoding="utf-8")
+
+print('[INFO] 调参说明:输入浮点数即可调参，输入非浮点数(比如空格)则保持原样')
+
+for line in lines:
+	if line.find("double k") != -1:
+		startPos = line.find('=') + 1
+		endPos = line.find(';')
+		print(f'[INFO] double k{line[startPos-2:endPos]}')
+		inputStr = input('>>> ')
+		try:	# 如果输入非数字，则保持原样
+			aug = float(inputStr)
+			file.write(line[:startPos] + str(aug) + line[endPos:])
+		except ValueError:
+			file.write(line + '\n')
+	else:
+		file.write(line + '\n')
+
+file.close()
